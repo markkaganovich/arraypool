903044d5f13ae6762fb3669d36f3f200f4897071
Mark Kaganovich <mark.kaganovich@gmail.com>
2012-02-27 15:32:57 -0800

    errors are a vector

--- vcfmatrix.R
+++ vcfmatrix.R
@@ -1,27 +1,30 @@
 snptotal = 7724854
 lines = 60
 
-Gceu <-as.matrix(read.table('~/1000GenomesData/CEU.low_coverage.2010_09.genotypes.vcfmatrixoutput',sep="\t", nrows = snptotal))[,-(lines+1)]
+G <-as.matrix(read.table('~/1000GenomesData/CEU.low_coverage.2010_09.genotypes.vcfmatrixoutput',sep="\t", nrows = snptotal))[,-(lines+1)]
 
 lcls.uniform <- rep(1/lines, lines)
-S.uniform <- Gceu %*% lcls.uniform
-lcls.beta <- solve(t(Gceu) %*% Gceu, t(Gceu) %*% S.uniform)
+S.uniform <- G %*% lcls.uniform
+lcls.beta <- solve(t(G) %*% G, t(G) %*% S.uniform)
 
 #generate random test.lcls vectors
 simlen <- 100
 linesensitivity <- c()
+freqs <- c(1/1000,1/100,1/10)
 for (j in 1:lines){
-    test.lcls <- rep(0,lines)
-    test.lcls[j] = 1/100
-    for (k in 1:simlen){
-	    i = sample(lines,1)
-	    if (i != j){
-            test.lcls[i] = sample(lines - sum(test.lcls), 1) 
-}
-}
+    errors <- c()
+    for (f in freqs){
+        test.lcls <- rep(0,lines)
+        test.lcls[j] = f
+        for (k in 1:simlen){
+	        i = sample(lines,1)
+	        if (i != j){
+                test.lcls[i] = sample(lines - sum(test.lcls), 1) 
+            }
+        }
 
 test.lcls <- test.lcls/lines
-S.test <- Gceu %*% test.lcls
+S.test <- G %*% test.lcls
 test.lcls.dist <- sum((test.lcls - lcls.uniform)^2)
 
 # simulate adding error, for each test.lcls
@@ -30,13 +33,13 @@ test.lcls.dist <- sum((test.lcls - lcls.uniform)^2)
 #errors <- c()
 #for (i in 1:10/50){
 std = .05
-S.error <- S.test + rnorm(snptotal, mean = 0, sd = std)
-test.beta.error <- solve(t(Gceu) %*% Gceu, t(Gceu) %*% S.error)
+S.error <- S.test + rnorm(snptotal, meai = 0, sd = std)
+test.beta.error <- solve(t(G) %*% G, t(G) %*% S.error)
 test.error.residual <- test.beta.error - test.lcls
-error <- sum(test.error.residual^2)
-linesensitivity[j] <- error
+errors <- c(errors,sum(test.error.residual^2))
+linesensitivity[j] <- errors
 #errors <-c(errors, sum(test.error.residual^2))
 #}
 }
-
+}
 
