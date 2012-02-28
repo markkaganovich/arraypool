f18103cdf86e5073d1d0b4f67194aebdcfa31108
Mark Kaganovich <mark.kaganovich@gmail.com>
2012-02-28 13:01:52 -0800

    added simulation component to calculating error2

--- vcfmatrix.R
+++ vcfmatrix.R
@@ -9,40 +9,42 @@ lcls.beta <- solve(t(G) %*% G, t(G) %*% S.uniform)
 
 #generate random test.lcls vectors
 simlen <- 100
-linesensitivity <- c()
+simlen2 <- 1000
+linesensitivity <- matrix(c(rep(0,180)), nrow=60, ncol=3)
 freqs <- c(1/1000,1/100,1/10)
 for (j in 1:lines){
     errors <- c()
-    for (f in freqs){
-        test.lcls <- rep(0,lines)
-        test.lcls[j] = f
-        for (k in 1:simlen){
-	        i = sample(lines,1)
-	        if (i != j){
-                test.lcls[i] = sample(lines - sum(test.lcls), 1) 
+    for (f in 1:length(freqs)){
+        simerrors <- c()
+        for (s in 1:simlen){
+            test.lcls <- rep(0,lines)
+            test.lcls[j] = freqs[f]
+            for (k in 1:simlen2){
+	            i = sample(lines,1)
+	            if (i != j){
+                    test.lcls[i] = sample(lines - sum(test.lcls), 1) 
+                }
             }
-        }
 
-    test.lcls <- test.lcls/lines
-    S.test <- G %*% test.lcls
-    test.lcls.dist <- sum((test.lcls - lcls.uniform)^2)
+        test.lcls <- test.lcls/lines
+        S.test <- G %*% test.lcls
+        test.lcls.dist <- sum((test.lcls - lcls.uniform)^2)
 
 # simulate adding error, for each test.lcls
 #test.beta <- solve(t(Gceu) %*% Gceu, t(Gceu) %*% S.test)
 #test.residual <- test.beta - test.lcls
 #errors <- c()
 #for (i in 1:10/50){
-    std = .05
-    simerror <- c()
-    for (k in 1:simlen){
+        std = .05
         S.error <- S.test + rnorm(snptotal, mean = 0, sd = std)
         test.beta.error <- solve(t(G) %*% G, t(G) %*% S.error)
         test.error.residual <- abs(test.beta.error[j] - test.lcls[j])/test.lcls[j]
         simerrors <- c(simerrors, test.error.residual)
-    }
-    error = sum(simerrors) / simlen
-    errors <- c(errors,error)
-    linesensitivity[j] <- errors
+    }        
+    error <- sum(simerrors) / simlen
+    linesensitivity[j , f] = error
+    print(j)
+    write(linesensitivity, "~/arraypool/test")
 #errors <-c(errors, sum(test.error.residual^2))
 #}
 }   
