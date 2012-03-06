d3f4f43b365d51f4b395f126e38a4011c77ccb44
markkaganovich <mark.kaganovich@gmail.com>
2012-03-06 14:58:09 -0800

    py

--- vcftoRmatrix.py
+++ vcftoRmatrix.py
@@ -1,17 +1,22 @@
-def getvcfmatrix(filename):
+def getvcfmatrix(filename, genomelist):
     file = open(filename)
     outputfile = open(filename+'matrixoutput', 'w')
-    total = 1
+    total = 0
     lines = file.readlines(1000000)
     while(lines!=[]):
         for line in lines:
             if not line.startswith('#'):
-                total=total+1
+                total= total+1
                 l = line.split('\t')
-                for g in l[9:]:
-                    matrixentry = int(g[0]) + int(g[2])
+                for g in genomeind:
+                    matrixentry = int(l[g][0]) + int(l[g][2])
                     outputfile.write(str(matrixentry) +'\t')
                 outputfile.write('\n')
+            elif line.startswith('#CHROM'):
+                l = line.strip('\n').split('\t')
+                genomeind = []
+                for genome in genomelist:
+                    genomeind.append(l.index('NA'+genome))
         lines = file.readlines(1000000)
         print total
     print total
