b0fad5d674a6471032b45c0141de29b9fd4f29e0
markkaganovich <mark.kaganovich@gmail.com>
2012-03-23 13:15:43 -0700

    latest vcfmatrix stuff

--- vcftoRmatrix.py
+++ vcftoRmatrix.py
@@ -1,5 +1,19 @@
 def hashvcf(vcffile):
     file = open(vcffile)
+    hash = {}
+    lines = file.readlines(10000000)
+    while(lines!= []):
+        for l in lines:
+            if not l.startswith('#'):
+                tokens = l.strip('\n').split('\t')
+                key = 'chr'+tokens[0]+'pos'+tokens[1]
+                hash[key] = []
+                for t in tokens[9:]:
+                    if t!='.':
+                        matrixentry = int(t[0])+int(t[2])
+                        hash[key].append(matrixentry)
+        lines = file.readlines(10000000)
+    return hash
 
 
 def getvcfmatrix(filename, genomelist):
