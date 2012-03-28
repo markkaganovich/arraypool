63a0a7f702eb82706097ba1ef5afe0289d88ed52
Mark Kaganovich <mark@labmeeting.com>
2012-03-28 12:29:56 -0700

    script

--- vcfhashscript.py
+++ vcfhashscript.py
@@ -1,9 +1,9 @@
 import simplejson
 import vcftoRmatrix 
 
-vcftoRmatrix.flatfilevcf('../1000Genomes/YRI.low_coverage.2010_09.genotypes.vcf', '../genotypes/YRIlowcov')
-vcftoRmatrix.flatfilevcf('../1000Genomes/CHBJPT.low_coverage.2010_09.genotypes.vcf', '../genotypes/CHBJPTlowcov')
-vcftoRmatrix.flatfilevcf('../1000Genomes/YRI.trio.2010_09.genotypes.vcf', '../genotypes/YRItrio')
-vcftoRmatrix.flatfilevcf('../1000Genomes/CEU.trio.2010_09.genotypes.vcf', '../genotypes/CEUtrio')
+vcftoRmatrix.flatfilevcf('../1000GenomesData/YRI.low_coverage.2010_09.genotypes.vcf', '../genotypes/YRIlowcov')
+vcftoRmatrix.flatfilevcf('../1000GenomesData/CHBJPT.low_coverage.2010_09.genotypes.vcf', '../genotypes/CHBJPTlowcov')
+vcftoRmatrix.flatfilevcf('../1000GenomesData/YRI.trio.2010_09.genotypes.vcf', '../genotypes/YRItrio')
+vcftoRmatrix.flatfilevcf('../1000GenomesData/CEU.trio.2010_09.genotypes.vcf', '../genotypes/CEUtrio')
 
 
