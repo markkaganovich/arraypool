eda62d65ea486f5c7d923fbe8e3bb2380ef249ca
Mark Kaganovich <mark.kaganovich@gmail.com>
2012-04-12 22:16:32 -0700

    make temp files

--- vcftoRmatrix.py
+++ vcftoRmatrix.py
@@ -6,23 +6,33 @@ class genotypes:
         self.snpfile = open('./' + name + 'SnpPos')
         self.linesfile = open('./' + name + 'Lines')
         self.genofile = open('./' + name + 'Geno')
-        self.tempoutput = open('./'+name+'tempoutput', 'w')
         self.__genolen__()
 
     def __selectSNPs__(self, chosenSNPs):
+        self.chosenoutput = open('./' + name + 'chosenoutput', 'w')
         self.snps = simplejson.load(self.snpfile)
         self.csindex = map(lambda x: self.snps.index(x), filter(lambda y: y in self.snps, chosenSNPs))
         self.havechosensnps = map(lambda x: self.snps[x], self.csindex)
+        del self.snps
 
     def __genolen__(self):
         l = self.genofile.readline()
         self.ln = len(l.split(','))
         self.genofile.seek(0)
         
-def combineflatgenos(names, chosenSNPs):
+def combineflatgenos(names, chosenSNPs = 'ALL'):
     files  = map(lambda x: genotypes(x), names)
     for f in files:
         f.__selectSNPs__(chosenSNPs)
+    
+    if chosenSNPs != 'ALL':
+        for f in files:
+            for c in f.csindex:
+                f.genofile.seek((c-1)*f.ln*2)
+                r = f.genofile.read(f.ln*2)
+                f.genofile.seek(0)
+                f.chosenoutput.write(r)
+    break
 
     unionchosensnps = []
     map(lambda x: unionchosensnps.extend(x.havechosensnps), files) 
