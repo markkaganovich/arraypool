21f741b5a35cbef4ba4ebf5e7a8ce8b84a56fa13
markkaganovich <mark.kaganovich@gmail.com>
2012-04-12 22:54:40 -0700

    achilles

--- vcftoRmatrix.py
+++ vcftoRmatrix.py
@@ -9,18 +9,17 @@ class genotypes:
         self.__genolen__()
 
     def __selectSNPs__(self, chosenSNPs):
-        self.chosenoutput = open('./' + name + 'chosenoutput', 'w')
+        self.chosenoutput = open('./' + self.name + 'chosenoutput', 'w')
         self.snps = simplejson.load(self.snpfile)
         self.csindex = map(lambda x: self.snps.index(x), filter(lambda y: y in self.snps, chosenSNPs))
         self.havechosensnps = map(lambda x: self.snps[x], self.csindex)
-        del self.snps
 
     def __genolen__(self):
         l = self.genofile.readline()
         self.ln = len(l.split(','))
         self.genofile.seek(0)
         
-def combineflatgenos(names, chosenSNPs = 'ALL'):
+def combineflatgenos(names, chosenSNPs):
     files  = map(lambda x: genotypes(x), names)
     for f in files:
         f.__selectSNPs__(chosenSNPs)
@@ -32,7 +31,8 @@ def combineflatgenos(names, chosenSNPs = 'ALL'):
                 r = f.genofile.read(f.ln*2)
                 f.genofile.seek(0)
                 f.chosenoutput.write(r)
-    break
+    f.chosenoutput.close()
+    return files
 
     unionchosensnps = []
     map(lambda x: unionchosensnps.extend(x.havechosensnps), files) 
