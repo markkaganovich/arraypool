31e0c872df71e0dbccaccaaea3cf0c90a1fb0c6e
Mark Kaganovich <mkagan@snively.stanford.edu>
2012-06-20 14:35:35 -0700

    working cell line selection

--- vcfhashscript.py
+++ vcfhashscript.py
@@ -10,12 +10,37 @@ from threading import Thread
 #vcftoRmatrix.flatfilevcf('../1000GenomesData/CEU.trio.2010_09.genotypes.vcf', '../genotypes/CEUtrio')
 
 
-file = open('./omnisnpsHASH')
-omnisnps = simplejson.load(file)
-file.close()
-snps = omnisnps.keys()
+#file = open('./omnisnpsHASH')
+#omnisnps = simplejson.load(file)
+#file.close()
+#snps = omnisnps.keys()
 
-vcftoRmatrix.combineflatgenos(['../genotypes/CEUlowcov','../genotypes/CHBJPTlowcov', '../genotypes/YRIlowcov', '../genotypes/CEUtrio', '../genotypes/YRItrio'], snps)
+
+names = ['../genotypes/CEUlowcov','../genotypes/CHBJPTlowcov', '../genotypes/YRIlowcov', '../genotypes/CEUtrio', '../genotypes/YRItrio']
+
+#vcftoRmatrix.combineflatgenos(names, snps)
+
+def getlines(names, genotypefile, chosenlines):
+    lines = []
+    for n in names:
+	LinesFile = open(n+'Lines')
+	lines.extend(simplejson.load(LinesFile))
+    inds = []
+    for c in chosenlines:
+	inds.append(lines.index(c))
+    inds = map(lambda x: x *2, inds)
+    file = open(genotypefile)
+    newgenofile = open('chosenlinesSfile','w')
+    genolines = file.readlines()
+    print inds
+    for l in genolines:
+	l = l.strip('\n')
+	newl = ','.join(map(lambda x: l[x], inds))
+	newgenofile.write(newl + '\n') 
+
+test = ["NA06985", "NA06986", "NA06994", "NA07000", "NA07037", "NA07051", "NA07346"]
+getlines(names, 'mergedoutput', test)    
+	
     
 
 
