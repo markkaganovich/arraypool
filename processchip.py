05f94bc56f2fb142ac1648f1e759739f7e726520
Mark Kaganovich <mark@labmeeting.com>
2012-06-22 17:07:57 -0700

    process array reports

--- processchip.py
+++ processchip.py
@@ -1,6 +1,6 @@
 import simplejson
 
-reportfile = 'MK1.txt'
+reportfile = 'MK3.txt'
 
 file = open(reportfile)
 lines = file.readlines()
@@ -16,12 +16,33 @@ omni18results={}
 
 for l in lines:
     tokens = l.split(',')
-    try:
-        omni18results[snps18id[tokens[0]]] = float(tokens[5])
-    except KeyError:
-        print tokens[0]
+    if tokens.__len__() < 3:
+        print l
+        print "WHAT THE HELL"
+    else:
+        try:
+            omni18results[snps18id[tokens[0]]] = float(tokens[5])
+        except KeyError:
+            print tokens[0]
 
-file = open('omni18results', 'w')
+file = open('omni18results'+reportfile, 'w')
 simplejson.dump(omni18results, file)
 file.close()
+
+
+omnisnps = simplejson.load(open('omnisnpmap18'))
+arrayfreq= []
+file = open('arrayBfreq'+reportfile, 'w')
+for s in omnisnps:
+    try: 
+        arrayfreq.append(omni18results[s])
+        file.write(str(omni18results[s])+',')
+    except KeyError:
+        print "not in array"
+        arrayfreq.append(0)
+        file.write('0,')
+
+file.close()
+
+
 
