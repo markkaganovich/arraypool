c501bd173385b1149612ba40efab21d0336c86b0
Mark Kaganovich <mkagan@snively.stanford.edu>
2012-06-29 11:31:11 -0700

    some new stuff

---
+++ exploreBAF.py
@@ -0,0 +1,43 @@
+import simplejson
+
+file = open('./MK1.txt')
+arraylines = file.readlines()
+file.close()
+
+
+file = open('./omniIDs')
+omniIDs = simplejson.load(file)
+file.close()
+
+complement=  {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
+
+# correct the REF:ALT issue
+# run through each snp on array, only keep the good ones, correct all the possible REF:ALT ones, 
+# store good ones that aren't in teach Geno RefAlt file until the next one is opened
+
+names = ['../genotypes/19CEUlowcov','../genotypes/19CHBJPTlowcov', '../genotypes/19YRIlowcov', '../genotypes/19CEUtrio', '../genotypes/19YRItrio']
+
+correctedarraylines = []
+notyet = []
+flipped = []
+correctedcomp = []
+flipcompl = []
+file = open(names[0]+'RefAlt')
+refalt = simplejson.load(file)
+file.close()
+for l in arraylines[11:]:
+    t = l.split(',')
+    id = t[0]
+    if t[9] != '-' and t[10] != '-':
+	alleleA = t[9]
+	alleleB = t[10]
+	if refalt[omniIDs[id]]['ref'] == alleleA and refalt[omniIDs[id]]['alt'] == alleleB:
+	    correctedarraylines.append(l)
+	elif refalt[omniIDs[id]]['ref'] == alleleB and refalt[omniIDs[id]]['alt'] == alleleA:
+	    flipped.append(l)
+	elif refalt[omniIDs[id]]['ref'] == complement[alleleB] and refalt[omniIDs[id]]['alt'] == complement[alleleA]:
+	    correctedcompl.append(l)
+	elif refalt[omniIDs[id]]['ref'] == complement[alleleA] and refalt[omniIDs[id]]['alt'] == complement[alleleB]:
+	    flipcompl.append(l)
+	else:
+	    notyet.append(l)
--- vcfrefaltscript.py
+++ vcfrefaltscript.py
@@ -1,8 +1,39 @@
 import simplejson
 import vcftoRmatrix
 
+'''
 vcftoRmatrix.getrefalt('../1000GenomesData/CEU.low_coverage.2010_09.genotypes.vcf', '../genotypes/19CEUlowcov')
 vcftoRmatrix.getrefalt('../1000GenomesData/YRI.low_coverage.2010_09.genotypes.vcf', '../genotypes/19YRIlowcov')
 vcftoRmatrix.getrefalt('../1000GenomesData/CHBJPT.low_coverage.2010_09.genotypes.vcf', '../genotypes/19CHBJPTlowcov')
 vcftoRmatrix.getrefalt('../1000GenomesData/YRI.trio.2010_09.genotypes.vcf', '../genotypes/19YRItrio')
 vcftoRmatrix.getrefalt('../1000GenomesData/CEU.trio.2010_09.genotypes.vcf', '../genotypes/19CEUtrio')
+
+
+'''
+names = ['../genotypes/19CEUlowcov','../genotypes/19CHBJPTlowcov', '../genotypes/19YRIlowcov', '../genotypes/19CEUtrio', '../genotypes/19YRItrio']
+
+inputs = ['../1000GenomesData/CEU.low_coverage.2010_09.genotypes.vcf', '../1000GenomesData/YRI.low_coverage.2010_09.genotypes.vcf', '../1000GenomesData/CHBJPT.low_coverage.2010_09.genotypes.vcf', '../1000GenomesData/YRI.trio.2010_09.genotypes.vcf', '../1000GenomesData/CEU.trio.2010_09.genotypes.vcf']
+
+
+for i in range(0,len(names)):
+    name = names[i]
+    input = inputs[i]
+    vcftoRmatrix.flatfilevcf(input,name)
+
+omniIDs = simplejson.load(open('./omni19IDs'))
+
+omniIDsrefalt = {}
+
+for name in names:
+    snppos = simplejson.load(open(name+'RefAlt'))
+    for id in omniIDs.keys():
+        omniIDsrefalt[id] = snppos[omniIDs[id]]
+    simplejson.dump(omniIDsrefalt, open(name+'RefAltIDs','w'))
+
+
+
+
+
+
+
+
