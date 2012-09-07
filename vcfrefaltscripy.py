d6b0b5079cb4c1bc7b05f4801ca96fe389c8cc5a
Mark Kaganovich <mark.kaganovich@gmail.com>
2012-09-06 22:56:09 -0400

    fixed indent

--- vcfrefaltscripy.py
+++ vcfrefaltscripy.py
@@ -12,14 +12,14 @@ outputalt = open(outputname + 'Alt','w')
 while(lines != []):
 	for l in lines:
 		if not l.startswith('#'):
-               tokens = l.strip('\n').split('\t')
-               f = filter(lambda x: 'GP' in x, tokens[7].split(';'))
-		if f != []:
-		    pos = 'chr'+f[0].split('=')[1].split(':')[0]+'pos'+f[0].split('=')[1].split(':')[1]               
-		    ref = tokens[3]
-		    alt = tokens[4]
-		    ref[pos] = ref
-			alt[pos] = alt
+			tokens = l.strip('\n').split('\t')
+			f = filter(lambda x: 'GP' in x, tokens[7].split(';'))
+			if f != []:
+		    	pos = 'chr'+f[0].split('=')[1].split(':')[0]+'pos'+f[0].split('=')[1].split(':')[1]               
+		    	ref = tokens[3]
+		    	alt = tokens[4]
+		    	ref[pos] = ref
+				alt[pos] = alt
 	lines = file.readlines(1000000)
 simplejson.dump(ref, outputref)
 simplejson.dump(alt, outputalt)
