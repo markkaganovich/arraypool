46c99fafd1640432d6cf518d58d545c02547fb14
Mark Kaganovich <mark.kaganovich@gmail.com>
2012-09-07 11:29:35 -0400

    hash

--- vcfrefaltscripy.py
+++ vcfrefaltscripy.py
@@ -4,8 +4,8 @@ outputname ='testceu'
 vcffile = '../1000GenomesData/CEU.low_coverage.2010_09.genotypes.vcf'
 
 file = open(vcffile)
-ref = {}
-alt = {}
+refhash = {}
+althash = {}
 lines = file.readlines(1000000)
 outputref = open(outputname + 'Ref','w')
 outputalt = open(outputname + 'Alt','w')
@@ -18,8 +18,8 @@ while(lines != []):
 				pos = 'chr'+f[0].split('=')[1].split(':')[0]+'pos'+f[0].split('=')[1].split(':')[1]
 				ref = tokens[3]
 				alt = tokens[4]
-				ref[pos] = ref
-				alt[pos] = alt
+				refhash[pos] = ref
+				althash[pos] = alt
 	lines = file.readlines(1000000)
-simplejson.dump(ref, outputref)
-simplejson.dump(alt, outputalt)
+simplejson.dump(refhash, outputref)
+simplejson.dump(althash, outputalt)
