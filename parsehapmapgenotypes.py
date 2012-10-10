115ed1724b658af24b069e2d1c9492dc0d27209c
Mark Kaganovich <mark.kaganovich@gmail.com>
2012-10-09 17:56:39 -0700

    parse

--- parsehapmapgenotypes.py
+++ parsehapmapgenotypes.py
@@ -1,13 +1,13 @@
 import simplejson
 
-def parsehapmapchrom(chrom):
-	people = ['NA19140','NA19154', 'NA19145','NA19173','NA19203','NA19206','NA19211','NA19222']
+def parsehapmapchrom(chrom, rshash, refalt={}):
+	people = ['NA19140','NA19154','NA19173','NA19203','NA19206','NA19211','NA19222']
 	filename = '../genotypes/hapmapchr'+str(chrom)
 	hf = open(filename)
 	lines = hf.readlines()
 	header = lines[0].split(' ')
 	out = open(filename+'genotype','w')
-	rsidout = open(filename+'posrsid','w')
+	posout = open(filename+'SnpPos','w')
 	rsidi = header.index('rs#')
 	chromi = header.index('chrom')
 	posi = header.index('pos')	
@@ -15,6 +15,8 @@ def parsehapmapchrom(chrom):
 	peoplewithgenos = filter(lambda x: x in header, people)
 	print peoplewithgenos
 	peoplei = map(lambda x: header.index(x), peoplewithgenos)
+	refalt = {}
+	refaltout = open(filename+'RefAlt', 'w')
 	for l in lines[1:]:
 		t = l.split(' ')
 		rsid = t[rsidi]
@@ -26,10 +28,30 @@ def parsehapmapchrom(chrom):
 		for p in peoplei:
 			t[p]
 			genotypes = map(lambda x: t[x], peoplei)
-			genocount = map(lambda x: len(filter(lambda y: alt == y, x)), genotypes)	
-			map(lambda x: out.write(str(x)+','), genocount[:-1])
-			out.write(str(genocount[-1])+'\n')
-			rsidout.write(rsid+'\n')
-	
+			genocount = map(lambda x: len(filter(lambda y: alt == y, x)), genotypes)
+			try:
+				SnpPos = rshash[rsid]
+				map(lambda x: out.write(str(x)+','), genocount[:-1])
+				out.write(str(genocount[-1])+'\n')
+				posout.write(SnpPos+'\n')
+				refalt[SnpPos]={}
+				refalt[SnpPos]['ref'] = ref
+				refalt[SnpPos]['alt'] = alt
+			except KeyError:
+				pass
+	simplejson.dump(refalt, refaltout)
+	return refalt
+			
+file = open('./hapmap_rsid_hash_lines')
+rsid2pos = {}
+rlines = file.readlines()
+for r in rlines:
+	t = r.split('\t')
+	rsid2pos[t[0]] = t[1].strip('\n')
+file.close()
+refalt = {}	
 for c in range(1,23):
-	parsehapmapchrom(c)
+	refalt = parsehapmapchrom(c, rsid2pos, refalt)
+file = open('../genotypes/hapmapRefAlt', 'w')
+simplejson.dump(refalt, file)
+file.close()
