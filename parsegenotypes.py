7145df40f8908029d03aef5a83b12ce12a58f786
Mark Kaganovich <mark.kaganovich@gmail.com>
2012-10-19 19:28:46 -0700

    checkRef function test

--- parsegenotypes.py
+++ parsegenotypes.py
@@ -6,26 +6,47 @@ def checkRef(Reffile, Altfile):
 	hg19 = worldbase.Bio.Seq.Genome.HUMAN.hg19(download = True)
 	Ref = simplejson.load(open(Reffile))
 	Alt = simplejson.load(open(Altfile))
-	Refcorrected = {}
-	Altcorrected = {}
 	flip = []
+	errors = []
 	keys = Ref.keys()
 	for snppos in keys:
 		t = snppos.split('pos')
 		hg19snp = str(hg19[t[0]][int(t[1])-1]).upper()
-		if hg19snp == Ref[snppos].upper():
-			Refcorrected[snppos] = Ref[snppos]
-			Altcorrected[snppos] = Alt[snppos]
-		elif hg19snp == Alt[snppos].upper():
-			Refcorrected[snppos] = Alt[snppos]
-			Altcorrected[snppos] = Ref[snppos]
+		refsnp = Ref[snppos].upper()
+		altnsp = Alt[snppos].upper()
+		if hg19snp == refsnp:
+			continue
+		elif hg19snp == altsnp:
+			Ref[snppos] = hg19snp
+			Alt[snppos] = Ref[snppos]
 			flip.append(snppos)
 		else:
 			print "Error: Neither Ref nor Alt of SNP corresponds to hg19 sequence"
-	return [Refcorrected, Altcorrected, flip]
+			errors.append(snppos)
+	file = open(Reffile+'flipped','w')
+	simplejson.dump(Ref, file)
+	file.close()
+	file = open(Altfile+'flipped','w')
+	simplejson.dump(Alt, file)
+	file.close()
+	return [flip, errors]
+	
+def flipGeno(genofile, flip):
+	lines = open(genofile).readlines()
+	newgeno = open(genofile+'flipped', 'w')
+	newgeno.write(lines[0])
+	for l in lines[1:]:
+		t = l.split('\t')
+		if t[0] in flip:
+			newg = map(lambda x: 2-int(x), l.split(','))
+			newl = t[0] +'\t' 
+			map(lambda x: newl + str(x) + ',')
+			newgeno.write(newl.strip('\n'))
+		else:
+			newgono.write(l)
 			
 a = checkRef('../genotypes/CEUlowcovRef', '../genotypes/CEUlowcovAlt')
-
+flipGeno('../genotypes/CEUlowcovGeno', a[0])
 
 #modified to the 19 version
 def parse1KGvcf(vcffile, outputname):
