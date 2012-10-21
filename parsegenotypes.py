37e7ceb4930dcd4939962997fe5beaf20ff9bf75
Mark Kaganovich <mark.kaganovich@gmail.com>
2012-10-21 14:58:49 -0700

    parsegenotypes.py

--- parsegenotypes.py
+++ parsegenotypes.py
@@ -47,7 +47,7 @@ def flipGeno(genofile, flip):
 		if t[0] in flip:
 			newg = map(lambda x: 2-int(x), t[1].split(','))
 			newl = t[0] +'\t' 
-			newl = reduce(lambda x,y: x+str(y) + ',', [newl].append(newg))
+			newl = reduce(lambda x,y: x+str(y) + ',', [newl]+newg)
 			newgeno.write(newl.strip('\n'))
 		else:
 			newgeno.write(l)
