7092ec32e044e1010b8c43bccb910c54c0c8a73c
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-01-09 00:51:16 -0500

    added os to processreport

--- processreport.py
+++ processreport.py
@@ -11,11 +11,11 @@ def splitreport(f, dir):
 			continue
 		if header:
 			i = header.index('Sample ID')
-			print l
 			outputfile = '25M' + l.split('\t')[i]
 			if l.split('\t')[i] != '3.1':
 				continue
 			else:
+				print l
 				if outputfile in os.listdir(dir):
 					out.write(l)
 				else:
