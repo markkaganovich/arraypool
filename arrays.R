85aaef5101fdfdbd174a9e24a8daaf3e8018d0cc
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-01-17 18:09:54 -0500

    working R array stuff

--- arrays.R
+++ arrays.R
@@ -1,18 +1,14 @@
-# R --no-restore --no-save --args pool2inlowcov.lowcov.geno Array25M3.Rinput Array25M4.Rinput d34 < arrays.R
+# R --no-restore --no-save --args pool2inlowcov.lowcov.geno Array25M3.Rinput .d34 Array25M4.Rinput Array25M5.Rinput < arrays.R
 #setwd('/Users/markkaganovich/arraypool/')
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 
 gfile = args[1]
 controlsnpfile = args[2]
-expsnpsfiles = args[4:length(args)]
 outputext = args[3]
+expsnpsfiles = args[4:length(args)]
 
-g.raw <- read.table(gfile,sep='\t', header=F, row.names=1)
-V2 = g.raw$V2
-V2 = levels(V2)[V2]
-V2 = as.matrix(V2)
-row.names(V2) = row.names(g.raw)
+g.raw <- read.table(gfile,sep=',', header=F, row.names=1)
 
 csnp.raw <- read.table(controlsnpfile, sep = '\t', header = F, row.names = 1)
 
@@ -20,23 +16,20 @@ csnp.raw <- read.table(controlsnpfile, sep = '\t', header = F, row.names = 1)
 if (length(expsnpsfiles) > 1){
 	for( ef in expsnpsfiles){
 		esnp.raw <- read.table(ef, sep = '\t', header = F, row.names = 1)
-		snps = intersect(row.names(na.omit(csnp.raw)), row.names(na.omit(esnp.raw)))
-		g = g.m[snps,]
-		csnp = csnp.raw[snps,]
-		esnp = esnp.raw[snps,]
+		snps = intersect(row.names(na.omit(csnp.raw)), row.names(na.omit(esnp.raw))
+		ind = rownames(g.raw) %in% snps 
+		g = g.raw[ind,]
+		ind.c = rownames(csnp.raw) %in% snps
+		csnp = csnp.raw[ind.c,]
+		ind.e = rownames(esnp.raw) %in% snps
+		esnp = esnp.raw[ind.e,]
 
 		d <- solve(t(g) %*% g, t(g) %*% (esnp - csnp))
 
 		output = paste(controlsnpfile,ef,outputext, sep='')
-		save(d, output)
+		save(d, file=output)
 
 	}
 }
 
 
-
-
-#print(gfile)
-#print(controlsnpfile)
-#print(expsnpsfiles[1])
-#print(outputext)
