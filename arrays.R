2808608459de240f7836a73a101b8805f3354881
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-01-16 20:33:51 -0500

    updated array.R with multiple file commandline

--- arrays.R
+++ arrays.R
@@ -1,26 +1,37 @@
+# R --no-restore --no-save --args pool2inlowcov.lowcov.geno Array25M3.Rinput Array25M4.Rinput d34 < arrays.R
 #setwd('/Users/markkaganovich/arraypool/')
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 
 gfile = args[1]
 controlsnpfile = args[2]
-expsnpsfile = args[3]
-output = args[4]
-
-#gfile = '30/poolgenotype.Rinput'
-#controlsnpfile = '30/newtheta1'
-#expsnpsfile = '30/newtheta3'
+expsnpsfiles = args[4:length(args)]
+outputext = args[3]
 
 g.raw <- read.table(gfile,sep='\t', header=F, row.names=1)
 csnp.raw <- read.table(controlsnpfile, sep = '\t', header = F, row.names = 1)
-esnp.raw <- read.table(expsnpsfile, sep = '\t', header = F, row.names = 1)
 
-snps = intersect(row.names(na.omit(csnp.raw)), row.names(na.omit(esnp.raw)))
 
-g = as.matrix(g.raw[snps,])
-csnp = csnp.raw[snps,]
-esnp = esnp.raw[snps,]
+if (length(expsnpsfiles) > 1){
+	for( ef in expsnpsfiles){
+		esnp.raw <- read.table(ef, sep = '\t', header = F, row.names = 1)
+		snps = intersect(row.names(na.omit(csnp.raw)), row.names(na.omit(esnp.raw)))
+		g = as.matrix(g.raw[snps,])
+		csnp = csnp.raw[snps,]
+		esnp = esnp.raw[snps,]
 
-d <- solve(t(g) %*% g, t(g) %*% (esnp - csnp))
-save(d, output)
+		d <- solve(t(g) %*% g, t(g) %*% (esnp - csnp))
 
+		output = paste(controlsnpfile,ef,outputext, sep='')
+		save(d, output)
+
+	}
+}
+
+
+
+
+#print(gfile)
+#print(controlsnpfile)
+#print(expsnpsfiles[1])
+#print(outputext)
