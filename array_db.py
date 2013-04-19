8309eb70c5239785f0610ef0c43158dfe9e20765
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-04-19 00:16:17 -0400

    explore arrays with db set up

--- array_db.py
+++ array_db.py
@@ -9,8 +9,17 @@ import commands
 import os
 import csv 
 
-
 db = create_engine('sqlite:///../cancergenomes/GENOTYPES.db', echo = True)
+
+Session = sessionmaker(db)
+session = Session()
+metadata = MetaData(db)
+
+kg_table_name = "1kg_lowcov_1.0"
+hapmap_table_name = "hapmap_raw_1.0"
+
+kg_table = Table(kg_table_name, metadata, autoload = True)
+hapmap_table = Table(hapmap_table_name, metadata, autoload = True)
 
 afile = open('mark/arraypool/25M1.1', 'r')
 lines = afile.readlines()
@@ -19,5 +28,5 @@ asnps = set([])
 for l in lines:
     asnps.add(l.split('\t')[9] + ':' + l.split('\t')[10])
 
-json.dump(asnps, open('array_snps', 'w'))
+json.dump(list(asnps), open('array_snps', 'w'))
 
