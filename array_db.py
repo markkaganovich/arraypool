d3c4cb596da8f9c81a03267dbe0d8d0da7f5a100
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-04-18 23:49:12 -0400

    explore arrays with db set up

--- array_db.py
+++ array_db.py
@@ -13,11 +13,11 @@ import headers
 db = create_engine('sqlite:///../cancergenomes/GENOTYPES.db', echo = True)
 
 afile = open('mark/arraypool/25M1.1', 'r')
-    lines = afile.readlines()
-    afile.close()
-    asnps = set([])
-    for l in lines:
-        asnps.add(l.split('\t')[9] + ':' + l.split('\t')[10])
+lines = afile.readlines()
+afile.close()
+asnps = set([])
+for l in lines:
+    asnps.add(l.split('\t')[9] + ':' + l.split('\t')[10])
 
 json.dump(asnps, open('array_snps'))
 
